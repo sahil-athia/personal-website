600cb25567fcc90354d7d2e7e01509b29fb6be4c
Sahil Athia <sahilathia918@gmail.com>
2021-02-04 22:11:57 -0500

    add vartical nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
   Link,
   Route
 } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 
 import About from "./pages/About";
 import Skills from "./pages/Skills";
@@ -13,11 +15,14 @@ import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
+    <>
+      <Nav defaultActiveKey="/" className="flex-column" style={{maxWidth: "10vw"}}>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/skills">Skills</Nav.Link>
+        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+      </Nav>
     <Router>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/skills">Skills</Link>
-      <Link to="/portfolio">Portfolio</Link>
 
       <div className="App">
         <Switch>
@@ -39,6 +44,7 @@ function App() {
         </Switch>
       </div>
     </Router>
+    </>
   );
 }
 
