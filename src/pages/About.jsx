ea89709a9c70eefdd19d2904000954f4aec7d2f7
Sahil Athia <sahilathia918@gmail.com>
2021-02-07 17:15:32 -0500

    add image and styling to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
+import "./About.scss";
+
 export default function About() {
   return (
-    <div>
+    <div className="about-box">
       <h1>About page</h1>
+      <div className="image-box">
+          <img src={"https://www.cadencemag.co.uk/wp-content/uploads/2018/04/2017314-city.jpg-resize_then_crop-_frame_bg_color_FFF-h_1365-gravity_center-q_70-preserve_ratio_true-w_2048_.jpg"} className="pic" alt="location"></img>
+        </div>
     </div>
   )
 }
