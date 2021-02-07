c955a209e37692fc0e1e05ec9abb781d73562495
Sahil Athia <sahilathia918@gmail.com>
2021-02-07 16:12:03 -0500

    format elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
   return(
     <div className="home-box">
       <div className="greeting">
-        Greeting text, name and title
+        <p>Greeting text,</p>
+        <p>name and</p> 
+        <p>title</p>
       </div>
       <div className="greeting-icon">
         Add art or image/animation to front home page
