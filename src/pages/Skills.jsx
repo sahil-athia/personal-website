ef502fe6f63886effa7644bdb01993b9bb5827ad
Sahil Athia <sahilathia918@gmail.com>
2021-02-07 16:19:23 -0500

    setup textscramble element in skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,15 @@
+import {TextScramble} from '../helpers/scramble.js'
+import React, { useEffect, useRef } from 'react'
+
 export default function Skills() {
+  const text = useRef(<></>);
   
   return(
     <div>
       <h1>Skills page</h1>
+      <div className="scatter">
+        <h4 className="text" ref={text} ></h4>
+      </div>
     </div>
   )
 }
