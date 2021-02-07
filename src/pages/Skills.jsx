3a8cd91d7696a6bba89123e0e6cd48a90aba223e
Sahil Athia <sahilathia918@gmail.com>
2021-02-07 16:22:03 -0500

    render text scramle on page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,7 +3,34 @@ import React, { useEffect, useRef } from 'react'
 
 export default function Skills() {
   const text = useRef(<></>);
-  
+
+  useEffect(() => {
+    const phrases = [
+      "Testing1",
+      "Testing2",
+      "Testing3",
+      "Testing4"
+    ]
+
+    const el = text
+    const fx = new TextScramble(el)
+    let timeOut;
+    let counter = 0
+    async function next() {
+      fx.setText(phrases[counter])
+        .then(() => {
+        timeOut = setTimeout(next, 1000)
+        })
+        .catch(e => console.log(e))
+      counter = (counter + 1) % phrases.length
+    } 
+    next()
+    
+    return function cleanup() {
+      clearTimeout(timeOut)
+    }
+  }, [])
+
   return(
     <div>
       <h1>Skills page</h1>
