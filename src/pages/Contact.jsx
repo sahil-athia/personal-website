cda4de35641193b47ba8130281bd16446c291870
Sahil Athia <sahilathia918@gmail.com>
2021-02-08 13:58:15 -0500

    add jquery events to contact box

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,27 @@
 import "./Contact.scss";
-import $ from 'jquery'
+import $ from 'jquery';
+import {useEffect} from"react";
 
 export default function Contact() {
+
+  useEffect(() => {
+      $(".contact-box").on({
+        mouseenter: function () {
+          $('.shape').addClass("shape-border");
+        },
+    
+        mouseleave: function () {
+          $('.shape').toggleClass('shape-border');
+        }
+    });
+  }, [])
+
   return (
     <div className="contact-box">
       <h1> CONTACT </h1>
       <div className="svg-wrapper">
         <svg height="60" width="520" xmlns="http://www.w3.org/2000/svg">
-        <rect className="shape shape-border" height="60" width="520" />
+        <rect className="shape" height="60" width="520" />
         <div className="text">Wolfsrudel Design</div>
         </svg>
     </div>
