df04cf96c63e9d1a6ba9ae1fced8a3b6752b7cc5
Sahil Athia <sahilathia918@gmail.com>
2021-02-08 13:51:09 -0500

    add border animation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,16 @@
 import "./Contact.scss";
+import $ from 'jquery'
 
 export default function Contact() {
   return (
     <div className="contact-box">
-      <h1> contact </h1>
+      <h1> CONTACT </h1>
+      <div className="svg-wrapper">
+        <svg height="60" width="520" xmlns="http://www.w3.org/2000/svg">
+        <rect className="shape shape-border" height="60" width="520" />
+        <div className="text">Wolfsrudel Design</div>
+        </svg>
+    </div>
     </div>
   )
 }
