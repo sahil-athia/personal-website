82a7948270050d7fc326f5544205f659b434a48c
Sahil Athia <sahilathia918@gmail.com>
2021-02-09 15:27:41 -0500

    center image and add side elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,21 @@ import "./Home.scss";
 export default function Home() {
   return(
     <div className="home-box">
-      <div className="greeting">
-        <p>Hi, Im Sahil Athia</p>
-      </div>
-      <div className="greeting-icon">
-        Add art or image/animation to front home page
-      </div>
+      
       <div className="title">
         <p>Web Developer | Beginner Blockchain Developer</p>
       </div>
+
+      <div className="greeting-box">
+        <span>SCROLL</span>
+        <img className="greeting-icon" src={"https://images.unsplash.com/photo-1496498808362-7e2ec3b323d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"}></img>
+        <span>BASED IN TORONTO</span>
+      </div>
       
+      <div className="greeting">
+        <p>Hi, Im Sahil Athia</p>
+      </div>
+
     </div>
   )
 }
