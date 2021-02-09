75d675323c414e92a7216d76dab7a92bcae12484
Sahil Athia <sahilathia918@gmail.com>
2021-02-09 15:46:14 -0500

    add image animation and menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,14 @@ import "./Home.scss";
 export default function Home() {
   return(
     <div className="home-box">
-      
-      <div className="title">
-        <p>Web Developer | Beginner Blockchain Developer</p>
+      <div className="menu">
+        <span>CONTACT</span>
+        <span>NAME</span>
+        <span>MENU</span>
+      </div>
+
+      <div className="greeting">
+        <p>HI, IM SAHIL ATHIA</p>
       </div>
 
       <div className="greeting-box">
@@ -13,11 +18,10 @@ export default function Home() {
         <img className="greeting-icon" src={"https://images.unsplash.com/photo-1496498808362-7e2ec3b323d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"}></img>
         <span>BASED IN TORONTO</span>
       </div>
-      
-      <div className="greeting">
-        <p>Hi, Im Sahil Athia</p>
-      </div>
 
+      <div className="title">
+        <p>JUNIOR WEB DEVELOPER</p>
+      </div>
     </div>
   )
 }
