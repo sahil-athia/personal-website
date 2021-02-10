b263bd7d3925e9b9aaf139fe1c27aa71290ac24d
Sahil Athia <sahilathia918@gmail.com>
2021-02-10 12:51:54 -0500

    add observer to use on view animations

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,30 @@
 import "./Contact.scss";
 import $ from 'jquery';
-import {useEffect} from"react";
+import {useEffect, useState, useRef} from"react";
 
 export default function Contact(props) {
+  const [isVisible, setVisible] = useState(true);
+  const domRef = useRef();
 
   useEffect(() => {
-      $(".contact-box").on({
-        mouseenter: function () {
-          $('.shape').addClass("shape-border");
-        },
-    
-        mouseleave: function () {
-          $('.shape').toggleClass('shape-border');
-        }
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
     });
-  }, [])
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
 
   return (
     <div className="contact-box" ref={props.contactPage}>
-      <span></span>
-      <h1></h1>
-      <hr></hr>
-      <div></div>
-      <hr></hr>
+      <span>GET IN TOUCH</span>
+      <h1 className="contact-header">CONTACT</h1>
+      <hr className={`line-1 ${isVisible ? 'slide' : ''}`} ref={domRef}></hr>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <hr className={`line-2 ${isVisible ? 'slide' : ''}`} ref={domRef}></hr>
       <span className="scroll-top" onClick={props.scrollToTop}>Back to Top</span>
     </div>
   )
