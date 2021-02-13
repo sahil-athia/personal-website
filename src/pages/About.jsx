1d8726e7b703465928f199da1d80e34f6ccf9b2b
Sahil Athia <sahilathia918@gmail.com>
2021-02-13 17:21:40 -0500

    add containers for the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,9 +20,12 @@ export default function About() {
 
   return (
     <div className="about-box">
-      <h1>About page</h1>
+      <h1 className="about-header">ABOUT</h1>
       <div className="image-box">
-          {/* <img src={"https://www.cadencemag.co.uk/wp-content/uploads/2018/04/2017314-city.jpg-resize_then_crop-_frame_bg_color_FFF-h_1365-gravity_center-q_70-preserve_ratio_true-w_2048_.jpg"} className="pic" alt="location"></img> */}
+        
+      </div>
+      <div className="about-text">
+
       </div>
     </div>
   )
