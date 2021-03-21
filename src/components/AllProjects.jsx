c723f6f2f912d5c83fb659d3493e82581bb5555f
Sahil Athia <sahilathia918@gmail.com>
2021-03-20 21:42:21 -0400

    update all portfolio components

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -7,6 +7,11 @@ export default function AllProjects() {
           <img className="project-img" src={"https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"}></img>
         </a>
 
+        <a target="_blank" href="https://github.com/sahil-athia/pet-shop-tutorial" rel="noreferrer">
+          {/* <p className="project-text"></p> */}
+          <img className="project-img" src={"https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/04/10/901440-cryptocurrency.jpg"}></img>
+        </a>
+
         <a target="_blank" href="https://github.com/sahil-athia/Jungle" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://magenticians.com/wp-content/uploads/2018/01/promote-your-online-store.jpg"} ></img>
@@ -30,11 +35,6 @@ export default function AllProjects() {
         <a target="_blank" href="https://github.com/sahil-athia/tinyapp" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://neilpatel.com/wp-content/uploads/2017/08/url.jpg"}></img>
-        </a>
-
-        <a target="_blank" href="https://github.com/sahil-athia/pet-shop-tutorial" rel="noreferrer">
-          {/* <p className="project-text"></p> */}
-          <img className="project-img" src={"https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/04/10/901440-cryptocurrency.jpg"}></img>
         </a>
 
         <a target="_blank" href="https://github.com/sahil-athia/defi-stake-app" rel="noreferrer">
