e874edf58d702f0b4546912118fdf5c549b6ce90
Sahil Athia <sahilathia918@gmail.com>
2021-03-20 21:36:09 -0400

    add security on links

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,27 +1,27 @@
 export default function AllProjects() {
   return (
     <div className="projects" data-aos="fade-up" data-aos-duration="1500">
-        <a target="_blank" href="https://github.com/sahil-athia/final-project">
+        <a target="_blank" href="https://github.com/sahil-athia/final-project" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"}></img>
         </a>
-        <a target="_blank" href="https://github.com/sahil-athia/Jungle">
+        <a target="_blank" href="https://github.com/sahil-athia/Jungle" rel="noreferrer">
           {/* <p className="project-text"></p> */}
-          <img className="project-img" src={"https://magenticians.com/wp-content/uploads/2018/01/promote-your-online-store.jpg"}></img>
+          <img className="project-img" src={"https://magenticians.com/wp-content/uploads/2018/01/promote-your-online-store.jpg"} ></img>
         </a>
-        <a target="_blank" href="https://github.com/sahil-athia/scheduler">
+        <a target="_blank" href="https://github.com/sahil-athia/scheduler" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://images.unsplash.com/photo-1515847049296-a281d6401047?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"}></img>
         </a>
-        <a target="_blank" href="https://github.com/sahil-athia/tweeter">
+        <a target="_blank" href="https://github.com/sahil-athia/tweeter" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://github.com/sahil-athia/tweeter/raw/master/docs/tweet-page.png?raw=true"}></img>
         </a>
-        <a target="_blank" href="https://github.com/JaredFlomen/midterm">
+        <a target="_blank" href="https://github.com/JaredFlomen/midterm" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://wallpaperaccess.com/full/1078877.jpg"}></img>
          </a>
-        <a target="_blank" href="https://github.com/sahil-athia/tinyapp">
+        <a target="_blank" href="https://github.com/sahil-athia/tinyapp" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://neilpatel.com/wp-content/uploads/2017/08/url.jpg"}></img>
         </a>
