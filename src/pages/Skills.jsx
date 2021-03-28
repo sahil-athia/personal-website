a145b373bfeca978f2e1c42901b4eb1c662b5acf
Sahil Athia <sahilathia918@gmail.com>
2021-03-27 22:07:09 -0400

    remove text scatter

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -17,73 +17,10 @@ export default function Skills() {
             $(".skills-box").removeClass("scrolled");
         }
     });
-
-    const phrases1 = [ 
-      "HTML", "JavaScript", "Ruby", "CSS", "Solidity"
-    ]
-    const phrases2 = [
-      "NodeJS", "ReactJS", "Ajax", "Express", "EJS", "jQuery", "Bootstrap", "SASS", "Rails"
-    ]
-    const phrases3 = [
-      "SQL", "Git", "Command Line"
-    ]
-
-    const el = [text1, text2, text3];
-    const [fx1, fx2, fx3] = [new TextScramble(el[0]), new TextScramble(el[1]), new TextScramble(el[2])];
-
-    let [timeOut1, timeOut2, timeOut3] = [null, null, null];
-    let [counter1, counter2, counter3] = [0, 0, 0];
-
-      const scramble1 = () => {
-        fx1.setText(phrases1[counter1])
-        .then(() => {
-        timeOut1 = setTimeout(scramble1, 1000)
-        })
-        .catch(e => console.log(e))
-        counter1 = (counter1 + 1) % phrases1.length;
-      }
-
-      const scramble2 = () => {
-        fx2.setText(phrases2[counter2])
-        .then(() => {
-        timeOut2 = setTimeout(scramble2, 1000)
-        })
-        .catch(e => console.log(e))
-        counter2 = (counter2 + 1) % phrases2.length;
-      }
-      
-    const scramble3 = () => {
-      fx3.setText(phrases3[counter3])
-        .then(() => {
-        timeOut3 = setTimeout(scramble3, 1000)
-        })
-        .catch(e => console.log(e))
-      counter3 = (counter3 + 1) % phrases3.length;
-    }
-
-      
-    // scramble1();
-    // scramble2();
-    // scramble3();
-    
-    return function cleanup() {
-      clearTimeout(timeOut1, timeOut2, timeOut3);
-    }
   }, [])
 
   return(
     <div className="skills-box">
-        {/* <div className="scatter">
-          <h4 className="text1" ref={text1} ></h4>
-        </div>
-
-        <div className="scatter">
-            <h4 className="text2" ref={text2} ></h4>
-          </div>
-
-          <div className="scatter">
-            <h4 className="text3" ref={text3} ></h4>
-          </div> */}
         <h1 className="skills-header">
           SKILLS
         </h1>
