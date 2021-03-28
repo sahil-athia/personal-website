9848201beb6a2987b2f5d6680c67f01286b88368
Sahil Athia <sahilathia918@gmail.com>
2021-03-27 22:16:20 -0400

    update links

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -7,7 +7,7 @@ export default function AllProjects() {
           <img className="project-img" src={"https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"}></img>
         </a>
 
-        <a target="_blank" href="https://github.com/sahil-athia/pet-shop-tutorial" rel="noreferrer">
+        <a target="_blank" href="https://github.com/sahil-athia/defi-stake-app" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/04/10/901440-cryptocurrency.jpg"}></img>
         </a>
@@ -37,7 +37,7 @@ export default function AllProjects() {
           <img className="project-img" src={"https://neilpatel.com/wp-content/uploads/2017/08/url.jpg"}></img>
         </a>
 
-        <a target="_blank" href="https://github.com/sahil-athia/defi-stake-app" rel="noreferrer">
+        <a target="_blank" href="https://github.com/sahil-athia/pet-shop-tutorial" rel="noreferrer">
           {/* <p className="project-text"></p> */}
           <img className="project-img" src={"https://i.guim.co.uk/img/media/40b168b7f39a3930919305307a4f96dc0befe8f2/6_0_10655_6400/master/10655.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=29a8e212cd68910d0ffdfd78e3ff73c2"}></img>
         </a>
